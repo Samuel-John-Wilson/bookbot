3f019b400c8c1d67fc777c8845ae7d42adafd910
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-04-25 00:26:06 +1000

    3: More functions more money

--- main.py
+++ main.py
@@ -4,8 +4,21 @@
 
 from stats import get_book_text
 from stats import word_count
+from stats import character_count
+from stats import get_only_alpha
+
+#def main(path_to_file):
+#    text = get_book_text(path_to_file)
+#    print(text)
+
+            
+
+#Franken_Count = character_count('books/frankenstein.txt')
+
+def print_counts(path_to_file):
+    print(f"{word_count(path_to_file)} Words found in the document")
+    print(f"Characters found: {character_count(path_to_file)}")
+
+print_counts('books/frankenstein.txt')
 
 
-
-
-word_count("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,4 +1,8 @@
 
+
+# dictionary containing the alphabet and symbols as keys
+
+
 # takes the file path and returns the file contents as a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -14,4 +18,31 @@ def word_count(path_to_file):
     file_contents = get_book_text(path_to_file)
     text = file_contents.split()
     count = len(text)
-    print(f"{count} words found in the document")
+    return count
+    
+    #print(f"{count} words found in the document")
+
+
+
+# Turns characters into lower case, then counts each one in a file    
+def character_count(path_to_file):
+    characters = {}
+    text = get_book_text(path_to_file).lower()
+    for char in text:
+        if char in characters:
+            characters[char] +=1
+        else:
+            characters[char] = 1
+    # Sort the dictionary by its keys and return as a new dictionary
+    sorted_characters = dict(sorted(characters.items()))
+    return sorted_characters        
+        
+def get_only_alpha(dic):
+    alpha_dic = {key: value for key, value in dic.items() if key.isalpha()}
+
+    return alpha_dic
+
+
+
+
+      
